63ce08b496fce21c4884d76e7e52da1adba01ebf
Jenny Ough <jenny.ough@ibm.com>
2018-10-20 12:59:43 +0100

    added SonarCloud scan

--- startup.sh
+++ startup.sh
@@ -8,4 +8,7 @@ npm run report
 cp --force report.json /mnt/report.json
 cp --force report.xml /mnt/report.xml
 cp --force report.json.html /mnt/report.json.html
+
+npm run sonarcloud
+
 exit 0 #Exit with success
