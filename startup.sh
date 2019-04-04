51f13787fa470de206e320949dce41eb058d11fc
Ubuntu <matthew@matthew-fsa-vm.v4d1dq0mhvke5n2u4f5khetsbh.cwx.internal.cloudapp.net>
2019-04-04 15:42:21 +0000

    Fixed errors in build process

--- startup.sh
+++ startup.sh
@@ -5,8 +5,4 @@ npm run load
 npm run xmlreport
 npm run report
 
-cp --force report.json /mnt/report.json
-cp --force report.xml /mnt/report.xml
-cp --force report.json.html /mnt/report.json.html
-
 exit 0 #Exit with success
